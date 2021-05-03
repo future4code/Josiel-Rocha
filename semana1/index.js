0398edc4d8c25e218cacaa22abdc5d24145a7491
JosielRM <josielmartins500@gmail.com>
2021-05-03 16:49:00 -0300

    html e js

---
+++ semana1/index.js
@@ -0,0 +1,39 @@
+/*
+lat a = 10
+let b = 10
+
+console.log(b) 
+
+b = 5 
+console.log(a, b) 
+
+Nos console (b) vai atribuir um valor 10 e no console (a, b) será (10, 5)
+*/
+
+/*
+let a = 10
+let b = 20
+let c 
+c = a =10
+b = c =10
+a = b =20
+
+console.log(a , b, c)
+Logo apos toda a dedução será atribuido console.log(10, 10, 20)
+*/
+
+/*
+let p = prompt("quantas horas você trabalha por dia?")
+let t = prompt("quanto você recebe por dia?")
+alert("você recebe $(t/p por hora")
+
+Roda uma perguntas com o let e o alerte sera prenchido com o let p e t.
+*/
+
+let nome = prompt ("Qual é o seu nome?")
+let idade = prompt ("Quantos anos você tem?")
+
+typeof nome // string
+typeof idade // number
+
+/* A prompt rodou com sucesso porem ocorrereu um erro no console*/
