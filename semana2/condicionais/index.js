4798677cab7aa388d357799a5479bc17afdf77a7
JosielRM <josielmartins500@gmail.com>
2021-05-12 16:26:25 -0300

    Execicios Aula 09

---
+++ semana2/condicionais/index.js
@@ -0,0 +1,12 @@
+// Exercios de interpretação e escrita de código
+//
+// Interpretação
+//
+
+if (numero % 2 === 0) {
+    console.log("Passou no teste.")
+  } else {
+    console.log("Não passou no teste.")
+  }
+cdrwqcr 
+webkitRTCPeerConnection = weqd
