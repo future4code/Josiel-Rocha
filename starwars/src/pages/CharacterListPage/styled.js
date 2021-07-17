01b736bf7ebcae8fe3f29b0149231f7def91c1dc
JosielRM <josielmartins500@gmail.com>
2021-07-17 15:13:35 -0300

    Atualização por conta de bugs

---
+++ starwars/src/pages/CharacterListPage/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export const characterCard = styled.div`
+    border: 1px solid black;
+    padding: 10px;
+    margin: 10px;
+    whith: 300px;
+    &:hover {
+        cursor: pointer;
+        background-color: lightblue;
+    }
+`
